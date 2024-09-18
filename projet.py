3559f83c6de1399a76aa0cb64a5c3504c2044d36
max-lycee-berthollet <146076358+max-lycee-berthollet@users.noreply.github.com>
2024-09-18 09:42:47 +0200

    Add files via upload

---
+++ projet.py
@@ -0,0 +1,12 @@
+class Pv:
+    '''
+s'occupe de la vie du personnage et des objets
+    '''
+    def __init__(self, categorie):
+        self.cat = categorie
+    def degats(self):
+        pass
+    
+class Arme:
+    def __init__(self):
+        self.pv = 
