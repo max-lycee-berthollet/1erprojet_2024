eff0eeacd5ef82a6d478a11751d532096be78f29
max-lycee-berthollet <146076358+max-lycee-berthollet@users.noreply.github.com>
2024-09-18 09:52:25 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -4,9 +4,14 @@ s'occupe de la vie du personnage et des objets
     '''
     def __init__(self, categorie):
         self.cat = categorie
+        self.pv = 100
     def degats(self):
-        pass
+        self.pv -= 4
+        return self.pv
     
 class Arme:
     def __init__(self):
-        self.pv = perso.Pv(perso)
+        self.pv = Pv(perso)
+    def attaque(self):
+        attaque = 0
+        return attaque
