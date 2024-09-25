0f93389a373115e7a504cabdf2f4745e971b2067
max-lycee-berthollet <146076358+max-lycee-berthollet@users.noreply.github.com>
2024-09-25 19:32:08 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -1,55 +1,3 @@
-print("Bienvenue dans un monde parallèle au notre! ")
-print("Tu es fils ainé de 16ans, avec des frères et soeurs.
-Un jour, en rentant de l'ecole, La lumière du soleil filtre à travers les rideaux déchirés.
-toute ta famille c'est fait tuer par ces monstres qui souhaite prendre le controle sur le monde.
-Ta petite soeur Lucie est la seule survivante de ta famille par miracle. 
-Malheureusement ta soeur c'est fait infecter par un de ces monstres. 
-Malgré le désespoir que tu puisses avoir, tu dois trouver un remède qui savère etre de baies magiques pour ta soeur qui ce trouve dans un village lointain,
-tout en survivant face aux démons et montres que tu pourrais rencontrer dans cette aventure.
-")
-
-name_hero=str(input("sasir le nom de ton personnage: "))
-print(name_hero,", es-tu prêt à sauver ta soeur ?")
-
-def fin():
-	print("L'histoire s'arrête là, et ta soeur va périr :-(")
-	exit()
-    """
-      fin:
-        quand cette fonction est appelé, le programme s'arrete
-    """
-def choix_debut():
-	print("1.Oui")
-	print("2.Non")
-	return int(input("Choisi en tapant un numéro: "))
-
-    """
-      choix_debut:
-	on doit choisir l'endroit où nous voulons aller
-	return (type): choisir l'endroit avec un numero
-    """
-
-choix1=choix_debut()
-if choix1==1:
-	print("l'histoire peut commencer!")
-else:
-	fin()
-	
-print(" ")
-def choix_direction():
-	print(name_hero,"ton aventure commence devant la porte de ta maison, où voudrais tu aller?")
-	print("1.aller en forêt, explorer les horizons")
-	print("2.aller en ville")
-	print("3.rester à la amison")
-	return int(input("choisi une direction:"))
-
-choix2 = choix_direction()
-if choix2==1:
-	print("Ok, tu pars route vers la forêt, accompagné de Julie")
-elif choix2==2:
-	print("Ok, tu pars en route vers la ville, accompagné de Julie")
-else:
-    fin()
 from random import randint
 class Pv:
     '''
@@ -100,6 +48,106 @@ class Bouclier:
         cat = [50,70,90,100,150,200]
         self.pv = Pv(cat[categorie]) 
 
+print("Bienvenue dans un monde parallèle au notre! ")
+print("Tu es fils ainé de 16ans, avec des frères et soeurs.
+Un jour, en rentant de l ecole, La lumière du soleil filtre à travers les rideaux déchirés.
+toute ta famille c est fait tuer par ces monstres qui souhaite prendre le controle sur le monde.
+Ta petite soeur Lucie est la seule survivante de ta famille par miracle. 
+Malheureusement ta soeur c est fait infecter par un de ces monstres. 
+Malgré le désespoir que tu puisses avoir, tu dois trouver un remède qui savère etre de baies magiques pour ta soeur qui ce trouve dans un village lointain,
+tout en survivant face aux démons et montres que tu pourrais rencontrer dans cette aventure.")
+
+name_hero=str(input("sasir le nom de ton personnage: "))
+print(name_hero,", es-tu prêt à sauver ta soeur ?")
+
+bourse = 100
+inventaire = []
+
+def fin():
+	print("L'histoire s'arrête là, et ta soeur va périr :-(")
+	exit()
+    """
+      fin:
+        quand cette fonction est appelé, le programme s'arrete
+    """
+def choix_debut():
+	print("1.Oui")
+	print("2.Non")
+	return int(input("Choisi en tapant un numéro: "))
+
+    """
+      choix_debut:
+	on doit choisir l'endroit où nous voulons aller
+	return (type): choisir l'endroit avec un numero
+    """
+
+choix1=choix_debut()
+if choix1==1:
+	print("l'histoire peut commencer!")
+else:
+	fin()
+	
+print(" ")
+def choix_direction():
+	print(name_hero,"ton aventure commence devant la porte de ta maison, où voudrais tu aller?")
+	print("1.aller en forêt, explorer les horizons")
+	print("2.aller en ville")
+	print("3.rester à la amison")
+	return int(input("choisi une direction:"))
+
+choix2 = choix_direction()
+if choix2==1:
+	print("Ok, tu pars route vers la forêt, accompagné de Julie")
+elif choix2==2:
+	print("Ok, tu pars en route vers la ville, accompagné de Julie")
+	ville()
+else:
+    fin()
+    
+def ville():
+    print('Te voila desormais en ville tu te trouves devant un magasin aisni que des maisons abandonnées')
+    print('Tu as soit le choix d aller acheter des armes et des boucliers dans l armurerie ou aller explorer les differentes maisons (1/2)')
+    choix = int(input('ton choix : '))
+    while choix != 1 or choix != 2:
+        print('ton choix n est pas valide choisit entre 1 ou 2')
+        choix = int(input('ton choix : '))
+    if choix == 1:
+        shop()
+    elif choix == 2:
+        maisons()
+        
+def shop():
+    print('Tu es rentré(e) dans l armurerie')
+    print(f'Tu as actuellement {bourse} dans ta bourse')
+    print('Tu as le choix entre acheter une arme ou un bouclier (1/2)')
+    choix = int(input('ton choix : '))
+    if choix == 1:
+        print('Tu as le choix entre plusieurs catégories d arme')
+        print('metal (50 d attaque, 80 pv, 10 sous),bronze (100 d attaque, 90 pv, 15 sous),argent (150 d attaque, 100 pv, 25 sous),or (200 d attaque, 150 pv, 35 sous),diamant (250 d attaque, 200 pv, 50 sous) (1/2/3/4/5)')
+        arme = int(input('ton choix : '))
+        if arme == 1:
+            bourse -= 10
+            inventaire.append('epée en metal')
+            
+    
+grades = {'metal':(50,80),'bronze':(100,90),'argent':(150,100), 'or':(200,150),'diamant':(250,200)}
+    
+    
+    
+    
+    
+    
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
